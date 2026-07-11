d7840f7c25f44bc3bcc0bc544c1b6f5c602692e0
Benoit Toussaint <toussaintbenoit1980@gmail.com>
2026-07-11 09:32:13 +0200

    Initialisation du projet Laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Blaster44</title>
+</head>
+<body style="background:#111;color:#8DFF2B;font-family:Arial;text-align:center;padding-top:100px;">
+
+<h1>Bienvenue chez Blaster44 🔫</h1>
+
+<p>Laravel fonctionne !</p>
+
+</body>
+</html>
