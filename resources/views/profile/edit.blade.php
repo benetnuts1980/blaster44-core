e7ffbc72671fd1894b2329e39fb995f0d54944da
Benoit Toussaint <TON_EMAIL_GITHUB>
2026-07-24 23:15:23 +0200

    User area completed and translated

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,35 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
+@section('title', 'Mon profil')
+
+@section('content')
+
+<div class="min-h-screen bg-[#0A0A0A] text-white py-32">
+
+    <div class="max-w-5xl mx-auto px-6">
+
+        <h1 class="text-5xl font-black mb-10">
+            👤 Mon <span class="text-lime-400">Profil</span>
+        </h1>
+
+        <div class="space-y-8">
+
+            <div class="bg-[#111111] border border-lime-400/20 rounded-3xl p-8 shadow-lg shadow-lime-500/10">
+                @include('profile.partials.update-profile-information-form')
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
+            <div class="bg-[#111111] border border-lime-400/20 rounded-3xl p-8 shadow-lg shadow-lime-500/10">
+                @include('profile.partials.update-password-form')
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
+            <div class="bg-[#111111] border border-red-500/20 rounded-3xl p-8 shadow-lg shadow-red-500/10">
+                @include('profile.partials.delete-user-form')
             </div>
+
         </div>
+
     </div>
-</x-app-layout>
+
+</div>
+
+@endsection
